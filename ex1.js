d769b96a22d0871f5c3f0ede38281e821ea5e1cc
QuyenThao <thaoquyen23012003@gmail.com>
2020-03-10 13:34:03 +0700

    ex1 commit

---
+++ ex1.js
@@ -0,0 +1,12 @@
+function tongHaiSo(a, b){
+    var c;
+    if(a === b){
+        c = (a + b)*3
+        console.log(c)
+    }else{
+        c = a + b 
+        console.log(c)
+    }
+}
+
+tongHaiSo(5,4)
